6ab005c1e0e3cbc311e867b99637429605460237
saragl728 <saragl728@gmail.com>
2024-01-09 10:26:22 +0100

    Habilito binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.proyectoclasessrpg"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.proyectoclasessrpg"
         minSdk = 24
