a81e7bfda53fa36d7bbec7e508b6618868e854d3
saragl728 <saragl728@gmail.com>
2024-01-09 11:58:29 +0100

    Añado código a la actividad principal

--- app/src/main/java/com/example/proyectoclasessrpg/MainActivity.kt
+++ app/src/main/java/com/example/proyectoclasessrpg/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.proyectoclasessrpg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.proyectoclasessrpg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //iniciar sesión
+        binding.bLogin.setOnClickListener {
+            //se comprueba si hay campos vacíos
+            if (binding.correo.text.toString().isNotEmpty() && binding.contrasenya.text.toString().isNotEmpty()){
+                //si no hay campos vacíos, debería intentar iniciar sesión
+            }
+            else{
+                //avisa si no hay un campo vacío
+                Toast.makeText(this, "Hay un campo vacío", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        //registrarse
+        binding.bRegistro.setOnClickListener {
+            //tiene que ir a la actividad de registro
+        }
     }
 }
