29d8091eccc0a316bee412ad84132b2e89329ffd
saragl728 <saragl728@gmail.com>
2024-01-09 11:54:32 +0100

    Añado una comprobación de campos vacíos a la actividad de registrar usuarios

--- app/src/main/java/com/example/proyectoclasessrpg/RegistroActivity.kt
+++ app/src/main/java/com/example/proyectoclasessrpg/RegistroActivity.kt
@@ -2,6 +2,7 @@ package com.example.proyectoclasessrpg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.proyectoclasessrpg.databinding.ActivityRegistroBinding
 
 class RegistroActivity : AppCompatActivity() {
@@ -11,7 +12,17 @@ class RegistroActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.botonRegistro.setOnClickListener {
-
+            //se comprueba si hay algo en blanco
+            if (binding.email.text.toString().isNotEmpty() && binding.contrasenya.text.toString()
+                    .isNotEmpty() && binding.nombreCompleto.text.toString()
+                    .isNotEmpty() && binding.nombreUsuario.text.toString().isNotEmpty()
+            ) {
+                //se intentará registrar la cuenta
+            }
+            //si se ha dejado algo en blanco, se avisa
+            else{
+                Toast.makeText(this, "Hay un campo en blanco", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
