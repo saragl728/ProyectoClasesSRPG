35798af4d69ca3dec07fffbb726bd2f950eed840
saragl728 <saragl728@gmail.com>
2024-01-17 09:33:53 +0100

    Termino la SplashActivity y hago que sea la actividad de inicio

--- app/src/main/java/com/example/proyectoclasessrpg/SplashActivity.kt
+++ app/src/main/java/com/example/proyectoclasessrpg/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.example.proyectoclasessrpg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.proyectoclasessrpg.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    public lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        screenSplash.setKeepOnScreenCondition{true}
+
+        //pausa de 3 segundos
+        Thread.sleep(3000)
+
+        //pasa a la actividad principal
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
+
 }
