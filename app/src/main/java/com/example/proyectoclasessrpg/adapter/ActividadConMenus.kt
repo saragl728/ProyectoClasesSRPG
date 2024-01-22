ae5aac2fe07748d4f1b6b8abac749bbbd1a44142
saragl728 <saragl728@gmail.com>
2024-01-22 10:37:51 +0100

    Se añade un menú con las demás opciones que implementaré y un adaptador para cambiar de actividad

---
+++ app/src/main/java/com/example/proyectoclasessrpg/adapter/ActividadConMenus.kt
@@ -0,0 +1,47 @@
+package com.example.proyectoclasessrpg.adapter
+
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectoclasessrpg.InfoActivity
+import com.example.proyectoclasessrpg.R
+
+class ActividadConMenus : AppCompatActivity() {
+    companion object {
+        var actividadActual = 0;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        //relacionamos la clase con el layout del menú que hennos creado
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        //desactivar la opción de la actividad en la que estamos
+        for (i in 0 until  menu.size()){
+            if (i == actividadActual) menu.getItem(i).isEnabled = false
+            else menu.getItem(i).isEnabled = true
+        }
+
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            //info te lleva a la actividad que muestra información sobre la aplicación
+            R.id.info -> {
+                val intent = Intent(this, InfoActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+                startActivity(intent)
+                true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+
+    }
+}
