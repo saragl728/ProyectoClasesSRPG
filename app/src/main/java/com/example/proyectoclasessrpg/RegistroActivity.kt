f628c04ebe905428e35960b531ad9fdee0057254
saragl728 <saragl728@gmail.com>
2024-01-24 09:17:32 +0100

    Añado una imagen a la actividad de inicio de sesión, pongo márgenes y quito una parte de la actividad de registro

--- app/src/main/java/com/example/proyectoclasessrpg/RegistroActivity.kt
+++ app/src/main/java/com/example/proyectoclasessrpg/RegistroActivity.kt
@@ -18,27 +18,27 @@ class RegistroActivity : AppCompatActivity() {
         binding.botonRegistro.setOnClickListener {
             //se comprueba si hay algo en blanco
             if (binding.email.text.toString().isNotEmpty() && binding.contrasenya.text.toString()
-                    .isNotEmpty() && binding.nombreCompleto.text.toString()
                     .isNotEmpty() && binding.nombreUsuario.text.toString().isNotEmpty()
             ) {
                 //se intentará registrar la cuenta
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(
                     binding.email.text.toString(), binding.contrasenya.text.toString()
-                ). addOnCompleteListener {
+                ).addOnCompleteListener {
                     //si funciona, irá a la actividad de la lista de clases
-                    if (it.isSuccessful){
+                    if (it.isSuccessful) {
                         val intent = Intent(this, ListadoClasesActivity::class.java)
                         startActivity(intent)
                     }
                     //si no se puede registrar el usuario, se mostrará un mensaje de error
-                    else{
-                        Toast.makeText(this, "No se ha podido añadir el usuario", Toast.LENGTH_LONG).show()
+                    else {
+                        Toast.makeText(this, "No se ha podido añadir el usuario", Toast.LENGTH_LONG)
+                            .show()
                     }
                 }
 
             }
             //si se ha dejado algo en blanco, se avisa
-            else{
+            else {
                 Toast.makeText(this, "Hay un campo en blanco", Toast.LENGTH_LONG).show()
             }
         }
