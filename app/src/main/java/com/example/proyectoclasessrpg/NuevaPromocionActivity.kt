dc905c13e5862c7e95ff33949756b112af2b68ec
saragl728 <saragl728@gmail.com>
2024-02-03 20:00:07 +0100

    Pongo código en la actividad de añadir promociones y hago pequeños cambios

--- app/src/main/java/com/example/proyectoclasessrpg/NuevaPromocionActivity.kt
+++ app/src/main/java/com/example/proyectoclasessrpg/NuevaPromocionActivity.kt
@@ -1,6 +1,7 @@
 package com.example.proyectoclasessrpg
 
 import android.os.Bundle
+import android.widget.Toast
 import com.example.proyectoclasessrpg.adapter.ActividadConMenus
 import com.example.proyectoclasessrpg.databinding.ActivityNuevaPromocionBinding
 
@@ -11,5 +12,21 @@ class NuevaPromocionActivity : ActividadConMenus() {
         setContentView(binding.root)
 
         title = "Añadir una promoción de clase"
+
+        binding.bAnyadirPromocion.setOnClickListener {
+            if (binding.claseBase.text.toString().isNotEmpty() && (binding.clasePromocionada.text.isNotEmpty())){
+                //comprueba que la clase base y la clase promocionadas no son iguales
+                if (binding.claseBase.text.toString() == binding.clasePromocionada.text.toString()){
+                    Toast.makeText(this, "La clase base no puede ser la misma que la clase promocionada", Toast.LENGTH_LONG).show()
+                }
+                //si no son iguales, intentará introducir la promoción
+                else{
+
+                }
+            }
+            else{
+                Toast.makeText(this, "Hay que rellenar ambos campos", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
