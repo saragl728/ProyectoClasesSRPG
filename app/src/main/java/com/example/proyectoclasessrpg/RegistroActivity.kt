f061a8e50542ae5c8bc0589e8237bad943c92375
saragl728 <saragl728@gmail.com>
2024-02-13 13:45:42 +0100

    Ahora se puede añadir una imagen de usuario. Un texto recomienda que sea pequeña. Se da acceso al almacenamiento

--- app/src/main/java/com/example/proyectoclasessrpg/RegistroActivity.kt
+++ app/src/main/java/com/example/proyectoclasessrpg/RegistroActivity.kt
@@ -3,12 +3,26 @@ package com.example.proyectoclasessrpg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.proyectoclasessrpg.databinding.ActivityRegistroBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class RegistroActivity : AppCompatActivity() {
+
+    //para el botón de añadir imagen
+    lateinit var imagen: ImageButton
+    lateinit var binding: ActivityRegistroBinding
+    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) {
+            imagen.setImageURI(uri)
+        } else {
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityRegistroBinding.inflate(layoutInflater)
@@ -16,6 +30,10 @@ class RegistroActivity : AppCompatActivity() {
 
         title = "Nuevo usuario"
 
+        //se intenta añadir una imagen
+        binding.botonImagen.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
 
         binding.botonRegistro.setOnClickListener {
             //se comprueba si hay algo en blanco
